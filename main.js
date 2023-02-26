b7588b20ac16d458c07ef14e2a02beaf74eb2266
Umar271108 <117084348+Umar271108@users.noreply.github.com>
2023-02-26 19:28:21 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+$(function(){
+    
+    $('.bike-slider').slick({
+        arrows:false,
+        dots:true,
+        fade:true,
+        autoplay:true,
+        autoplaySpeed:2000,
+    });
+
+})
+
+$(function(){
+
+    $('.big-bike-slider').slick({
+        arrows:false,
+        dots:false,
+        fade:true,
+        autoplay:true,
+        autoplaySpeed:2000
+    }
+    )
+})
